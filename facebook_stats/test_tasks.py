e94ad9d9fe817a15276539d5ec557f5942cbeaf9
Jan Chmelicek <jan.chmelicek@simplybusiness.co.uk>
2016-06-22 12:15:06 +0100

    Refactor tests

--- facebook_stats/test_tasks.py
+++ facebook_stats/test_tasks.py
@@ -1,6 +1,6 @@
 import unittest
-from unittest import mock
 import redis
+from unittest import mock
 from httmock import HTTMock, all_requests
 
 import facebook_stats.tasks as tasks
@@ -9,47 +9,69 @@ from facebook_stats.comments import Comments
 
 class TestComments(unittest.TestCase):
 
-    def test_load_batch(self):
+    def setUp(self):
         tasks.redis_conn = redis.Redis(decode_responses=True)
         tasks.before_task()
         tasks.comments = Comments(tasks.redis_conn, None, tasks.REDIS_KEY_PATTERN)
+
+    def test_load_batch(self):
+
+        facebook_mock = FacebookMock()
         with mock.patch('celeryconfig.CELERY_ALWAYS_EAGER', True, create=True):
-            with HTTMock(self._facebook_mock):
+            with HTTMock(facebook_mock.execute):
                 tasks.load_batch.apply(args=('https://graph.facebook.com/v2.6/51752540096_10151775534413086/comments?fields=created_time&filter=stream&limit=1',)).get()
 
     def test_load_batch_with_sleep(self):
-        self._sent_error = False
-        tasks.redis_conn = redis.Redis(decode_responses=True)
-        tasks.before_task()
-        tasks.comments = Comments(tasks.redis_conn, None, tasks.REDIS_KEY_PATTERN)
+        facebook_sleep_mock = FacebookSleepMock()
         with mock.patch('celeryconfig.CELERY_ALWAYS_EAGER', True, create=True):
-            with HTTMock(self._facebook_sleep_mock):
+            with HTTMock(facebook_sleep_mock.execute):
                 tasks.load_batch.apply(args=('https://graph.facebook.com/v2.6/51752540096_10151775534413086/comments?fields=created_time&filter=stream&limit=1',)).get()
 
-    # FIXME: Refactor
+    def tearDown(self):
+        tasks.redis_conn = None
+        tasks.comments = None
+
+class FacebookMock(object):
+
     @all_requests
-    def _facebook_mock(self, url, request):
-        raw_params = url.query.split('&')
-        params = {name: value for (name, value) in (p.split('=') for p in raw_params)}
+    def execute(self, url, request):
+        params = self._get_params(url)
 
         if 'after' not in params:
-            return """{
-              "data": [
-                {
-                  "created_time": "2013-07-09T14:12:33+0000",
-                  "id": "10151775534413086_29777454"
-                }
-              ],
-              "paging": {
-                "cursors": {
-                  "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREExTmpnd09EWTZANamszTnpjME5UUT0ZD",
-                  "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREExTmpnd09EWTZANamszTnpjME5UUT0ZD"
-                },
-                "next": "https://graph.facebook.com/v2.6/51752540096_10151775534413086/comments?fields=created_time&filter=stream&limit=1&after=WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREExTmpnd09EWTZANamszTnpjME5UUT0ZD"
-              }
-            }"""
+            return self._first_response()
         elif params['after'] == 'WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREExTmpnd09EWTZANamszTnpjME5UUT0ZD':
-            return """{
+            return self._next_response()
+        elif params['after'] == 'WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREEyTlRNd09EWTZANamszTnpjME5UZAz0ZD':
+            return self._last_response()
+        else:
+            raise RuntimeError('Unknown url')
+
+    @staticmethod
+    def _get_params(url):
+        raw_params = url.query.split('&')
+        return {name: value for (name, value) in (p.split('=') for p in raw_params)}
+
+    @staticmethod
+    def _first_response():
+       return """{
+          "data": [
+            {
+              "created_time": "2013-07-09T14:12:33+0000",
+              "id": "10151775534413086_29777454"
+            }
+          ],
+          "paging": {
+            "cursors": {
+              "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREExTmpnd09EWTZANamszTnpjME5UUT0ZD",
+              "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREExTmpnd09EWTZANamszTnpjME5UUT0ZD"
+            },
+            "next": "https://graph.facebook.com/v2.6/51752540096_10151775534413086/comments?fields=created_time&filter=stream&limit=1&after=WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREExTmpnd09EWTZANamszTnpjME5UUT0ZD"
+          }
+        }"""
+
+    @staticmethod
+    def _next_response():
+        return """{
               "data": [
                 {
                   "created_time": "2013-07-10T14:12:33+0000",
@@ -64,8 +86,10 @@ class TestComments(unittest.TestCase):
                 "next": "https://graph.facebook.com/v2.6/51752540096_10151775534413086/comments?fields=created_time&filter=stream&limit=2&after=WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREEyTlRNd09EWTZANamszTnpjME5UZAz0ZD"
               }
             }"""
-        elif params['after'] == 'WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREEyTlRNd09EWTZANamszTnpjME5UZAz0ZD':
-            return """{
+
+    @staticmethod
+    def _last_response():
+        return """{
               "data": [
                 {
                   "created_time": "2013-07-10T14:12:33+0000",
@@ -80,31 +104,45 @@ class TestComments(unittest.TestCase):
               }
             }"""
 
-    # FIXME: Refactor
+class FacebookSleepMock(object):
+
+    def __init__(self):
+        self._sent_error = False
+
     @all_requests
-    def _facebook_sleep_mock(self, url, request):
+    def execute(self, url, request):
         if not self._sent_error:
             self._sent_error = True
-            return """{
-                "error":
-                {
-                    "code": 4
-                }
-                }"""
+            return self._sleep_response()
         else:
-             return """{
-              "data": [
-                {
-                  "created_time": "2013-07-10T14:12:33+0000",
-                  "id": "10151775534413086_29777454"
-                }
-              ],
-              "paging": {
-                "cursors": {
-                  "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREExTmpnd09EWTZANamszTnpjME5UUT0ZD",
-                  "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREEyTlRNd09EWTZANamszTnpjME5UZAz0ZD"
-                }
-              }
-            }"""
+            return self._last_response()
+
+
+    @staticmethod
+    def _sleep_response():
+        return """{
+                "error":
+                    {
+                        "code": 4
+                    }
+                }"""
+
+    @staticmethod
+    def _last_response():
+     return """{
+      "data": [
+        {
+          "created_time": "2013-07-10T14:12:33+0000",
+          "id": "10151775534413086_29777454"
+        }
+      ],
+      "paging": {
+        "cursors": {
+          "before": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREExTmpnd09EWTZANamszTnpjME5UUT0ZD",
+          "after": "WTI5dGJXVnVkRjlqZAFhKemIzSTZANVEF4TlRFM056VTFOREEyTlRNd09EWTZANamszTnpjME5UZAz0ZD"
+        }
+      }
+    }"""
+
 if __name__ == '__main__':
     unittest.main()
